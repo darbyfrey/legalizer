81732b888112bba4a8b9b0ccb9015cc7031dc1a3
Darby Frey <darbyfrey@gmail.com>
2010-12-10 16:32:16 -0800

    fixing bug with template sending method

--- lib/legalizer/document/template.rb
+++ lib/legalizer/document/template.rb
@@ -5,10 +5,14 @@ module Legalizer
       @source = options[:object]
     end
 
-    def send(request_xml)
+    # prepare a template for sending, return the guid of the prepared instance
+    def package
       template_guid = self.source["guid"]
       package = @config.connection.request(:get, "/api/templates/#{template_guid}/prepackage.json", @config.token, {})
-      package_guid = JSON.parse(package.body)["template"]["guid"]
+      JSON.parse(package.body)["template"]["guid"]
+    end
+
+    def send(request_xml)
       response = JSON.parse(@config.connection.request(:post, '/api/templates.json', @config.token, {}, request_xml, { 'Content-Type' => 'application/xml' }).body)
       response["document"]["guid"]
     end
